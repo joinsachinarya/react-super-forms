a4446a8db6ac948f59ad224538c426fbf2d39924
sac6inarya <sachinkarya333@gmail.com>
2023-02-13 00:40:25 +0530

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import React  from "react";
 import "./App.scss";
-import RegistrationForm from "./RegistrationForm";
-import PlaceDetailsForm from "./PlaceDetailsForm";
-
 function App() {
 
   return (
     <div className="App">
-        <PlaceDetailsForm />
-        <RegistrationForm />   
+         
     </div>
   );
 }
