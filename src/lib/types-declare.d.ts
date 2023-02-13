701596731b1034c5b5147b6e6483b9a60c593ee8
Adnan Husain <husainadnan15@gmail.com>
2023-02-13 20:29:05 +0530

    added declarations

---
+++ src/lib/types-declare.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+declare global {
+  namespace React {
+    /** Fixes React 18 compatibility issues with formik: https://github.com/jaredpalmer/formik/issues/3546#issuecomment-1127014775 */
+    type StatelessComponent<P> = React.FunctionComponent<P>;
+  }
+}
